5414a431958633a398ac26db15540bea240e3707
AmiraHasfarisha <hasfarisha@gmail.com>
2018-09-04 16:05:46 +0800

    hanges to be committed: 	modified:   echo1.c

--- echo1.c
+++ echo1.c
@@ -7,5 +7,6 @@ int main()
 	printf("Name:MRY\nGroup:CS2454A\n\n");
 	printf("Name:Fatiqah12\nGroup:CS2454A\n\n");
 	printf("Name:eyma\nGroup:CS2454A\n\n");
+	printf("Name:AmiraHasfarisha\nGroup:CS2454A\n\n");
 	return 0;
 }
