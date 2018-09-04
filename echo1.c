09fc51860f702c652e6d4ed1cee52c9b96f6ca8c
MieyszEyma <zaimahjamaluddin96@yahoo.com>
2018-09-04 15:59:44 +0800

    d:   echo1.c

--- echo1.c
+++ echo1.c
@@ -6,5 +6,6 @@ int main()
 	printf("Name:Shahadan\nGroup:CS2454A\n\n");
 	printf("Name:MRY\nGroup:CS2454A\n\n");
 	printf("Name:Fatiqah12\nGroup:CS2454A\n\n");
+	printf("Name:eyma\nGroup:CS2454A\n\n");
 	return 0;
 }
